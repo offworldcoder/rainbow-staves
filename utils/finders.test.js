94ee3b90c4687c12f57f22e5ee2ed8dea0500a5f
milesl <lee.miles@sky.uk>
2021-02-02 07:50:06 +0000

    added third unit test

--- utils/finders.test.js
+++ utils/finders.test.js
@@ -18,6 +18,7 @@ test('cut out one note on one stave', () => {
         ]
     )
 })
+
 test('cut out two notes on one stave', () => {
     let notes = [
         _note(100, 200, 20, 20),
@@ -38,6 +39,52 @@ test('cut out two notes on one stave', () => {
             {"height": 2, "real": true, "width": 100, "x": 0, "y": 200},
             {"height": 2, "real": true, "width": 180, "x": 120, "y": 200},
             {"height": 2, "real": true, "width": 680, "x": 320, "y": 200}
+        ]
+    )
+})
+
+test('cut out two notes on one stave and second stave with no notes', () => {
+    let notes = [
+        _note(100, 200, 20, 20),
+        _note(300, 200, 20, 20)
+    ]
+
+    let staves = [
+        _stave(0, 200, 1000, 2),
+        _stave(0, 220, 1000, 2)
+    ]
+    
+    let src = null
+    expect(cutoutNotesFromStaves(staves, notes, src)).toStrictEqual(
+        [
+            {"height": 2, "real": true, "width": 100, "x": 0, "y": 200},
+            {"height": 2, "real": true, "width": 180, "x": 120, "y": 200},
+            {"height": 2, "real": true, "width": 680, "x": 320, "y": 200},
+            {"height": 2, "width": 1000, "x": 0, "y": 220},
+        ]
+    )
+})
+
+test('cut out two notes on one stave and second,thirds staves with no notes', () => {
+    let notes = [
+        _note(100, 200, 20, 20),
+        _note(300, 200, 20, 20)
+    ]
+
+    let staves = [
+        _stave(0, 180, 1000, 2),
+        _stave(0, 200, 1000, 2),
+        _stave(0, 220, 1000, 2)
+    ]
+    
+    let src = null
+    expect(cutoutNotesFromStaves(staves, notes, src)).toStrictEqual(
+        [
+            {"height": 2, "width": 1000, "x": 0, "y": 180},
+            {"height": 2, "real": true, "width": 100, "x": 0, "y": 200},
+            {"height": 2, "real": true, "width": 180, "x": 120, "y": 200},
+            {"height": 2, "real": true, "width": 680, "x": 320, "y": 200},
+            {"height": 2, "width": 1000, "x": 0, "y": 220},
         ]
     )
 })
