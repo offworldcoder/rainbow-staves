88822d3f1f293d485dcf1b4077e3d75843916be1
milesl <lee.miles@sky.uk>
2021-02-02 07:47:16 +0000

    added second unit test

--- utils/finders.test.js
+++ utils/finders.test.js
@@ -18,6 +18,29 @@ test('cut out one note on one stave', () => {
         ]
     )
 })
+test('cut out two notes on one stave', () => {
+    let notes = [
+        _note(100, 200, 20, 20),
+        _note(300, 200, 20, 20)
+    ]
+
+    let staves = [
+        _stave(0, 200, 1000, 2)
+    ]
+    
+    // ----------O----------O----------|
+    //          100        300         1000
+    //       100, 120 + 180, 320 + 680
+
+    let src = null
+    expect(cutoutNotesFromStaves(staves, notes, src)).toStrictEqual(
+        [
+            {"height": 2, "real": true, "width": 100, "x": 0, "y": 200},
+            {"height": 2, "real": true, "width": 180, "x": 120, "y": 200},
+            {"height": 2, "real": true, "width": 680, "x": 320, "y": 200}
+        ]
+    )
+})
 
 function _note(x, y, cols, rows) {
     return {
